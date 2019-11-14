a82c9c8dccc83265a1f1baf077f0f87074745930
Jinzhubin <51435018+wsndyooo@users.noreply.github.com>
2019-11-14 20:51:48 +0800

    matri.h
    矩阵乘法加法头文件

---
+++ matri.h
@@ -0,0 +1,9 @@
+#ifndef __FFT_H__
+#define __FFT_H__
+
+#define PI 3.1415926535897932384626433832795028841971 /*圆周率 */
+#define N 3 /*定义复数类型 */ 
+void matrix_mul(double *A, double *B, double *C, int x, int z, int y);
+void matrix_add(double *A, double *B, double *C, int x, int y);
+
+#endif
